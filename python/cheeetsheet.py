57303b22da58c54f7732a5edb7af93ec31ce57e6
Kumamoto <onioni9999nagare@gmail.com>
2020-03-06 16:06:34 +0900

    add cheetsheet for python, git

---
+++ python/cheeetsheet.py
@@ -0,0 +1,27 @@
+# dict
+d = {"key1":1, "key2":2, "key3":3}
+# dict for
+for k in d:
+    print(k)
+    # key1
+    # key2
+    # key3
+# dict for key
+for k in d.keys:
+    print(k)
+    #上に同じ
+# dict for values
+for v in d.values:
+    print(v)
+    # 1
+    # 2
+    # 3
+for k, v in d.items:
+    print(k, v)
+    # key1 1
+    # key2 2
+    # key3 3
+
+
+
+
